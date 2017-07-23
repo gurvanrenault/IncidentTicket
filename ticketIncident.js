4bfcde7cade2ae5ed5876476d1b4cae89a9c4f1b
Gurvan Renault <renault.gurvan@gmail.com>
2017-07-23 19:16:57 +0200

    Add index.html and ticketIncident.js

---
+++ ticketIncident.js
@@ -0,0 +1,29 @@
+//
+function newTicket(object)
+{
+  var priority_class_name;
+  console.log(object["priority"]);
+  if (typeof(object["id"])!="number"){
+    throw new TypeError("The 'id' parameter should be a number , "+typeof(object["id"])+" given");
+  }
+
+  //
+  switch (object["priority"]) {
+    case "Low":
+      priority_class_name="low_priority";
+      break;
+    case "Medium":
+      priority_class_name="medium_priority";
+      break;
+    case "High":
+      priority_class_name="high_priority";
+      break;
+    default:
+      throw new  RangeError("The 'priority' parameter should be 'Low','Medium' or 'High'");
+  }
+  //
+  var ticket = document.createElement("ticket");
+  ticket.innerHTML = "<div class='ticket_body "+priority_class_name+"'></div>";
+  document.body.appendChild(ticket);
+
+}
